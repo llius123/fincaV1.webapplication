4865b7ad69b7632624ab8cac92860be60c1c4b1d
llius <llius619@gmail.com>
2019-02-15 20:37:12 +0100

    Listar facturas usuario

--- src/app/usuario/facturas/facturas.component.ts
+++ src/app/usuario/facturas/facturas.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { FacturaService } from 'src/app/service/factura/factura.service';
+import { Router } from '@angular/router';
+import * as moment from 'src/assets/moment-with-locales.js';
+import { PdfService } from 'src/app/service/pdf.service';
+import { ConfigService } from 'src/app/service/config/config.service';
 
 @Component({
   selector: 'app-facturas',
@@ -8,18 +12,143 @@ import { FacturaService } from 'src/app/service/factura/factura.service';
 })
 export class FacturasComponent implements OnInit {
 
-  constructor(private sql: FacturaService) { }
+  constructor(private sql: FacturaService, private router: Router, private pdfService: PdfService,private config: ConfigService) { }
 
   facturas: FacturaProveedorInterface[];
+  oFiltro: boolean = false;
+
+  //proveedorSeleccionado: ProveedorInterface = { id: null, direccion: null, telefono: null, email: null, poblacion: null };
+  proveedorSeleccionado = '';
+  display_proveedor: boolean = false;
+
+  comunidadSeleccionado = '';
+  display_comunidad: boolean = false;
+
+  tipofacturas: TipofacturaInterface[];
+  proveedores: ProveedorInterface[];
+
+  es: any;
+
+  //NgModel bindeados
+  tipofacturaSelect: any;
+  cobradoSelect: any;
+  desdeFecha: Date;
+  hastaFecha: Date;
+  proveedorSelect: any;
 
   datos: boolean = false;
   ngOnInit() {
+    this.getAll();
+  }
+
+  getAll(){
+    this.es = this.config.formatoFechaDatePicker;
     this.sql.getAll2().subscribe(
       data => {
         this.facturas = data
         this.datos = true;
       }
     )
+  }
+
+  selected(data: any, table: string) {
+    switch (table) {
+      case 'proveedor':
+        this.proveedorSeleccionado = data;
+        this.display_proveedor = true;
+        this.display_comunidad = false;
+        break;
+      case 'comunidad':
+        this.comunidadSeleccionado = data;
+        this.display_proveedor = false;
+        this.display_comunidad = true;
+        break;
+    }
+  }
+
+  filtro() {
+    if (this.oFiltro == true) {
+      this.oFiltro = false;
+    } else {
+      this.oFiltro = true;
+    }
+  }
+
+  onChange(tabla: string) {
+    switch (tabla) {
+      case 'cobrado':
+        if (this.cobradoSelect != 0) {
+          this.sql.filtroGeneral('cobrado', this.cobradoSelect).subscribe(
+            data => this.facturas = data
+          )
+          this.tipofacturaSelect = 0;
+          this.desdeFecha = null;
+          this.hastaFecha = null;
+          this.proveedorSelect = 0;
+        } else {
+          this.getAll()
+        }
+        break;
+      case 'tipofactura':
+        if (this.tipofacturaSelect > 0) {
+          this.sql.filtroGeneral('id_tipofactura', this.tipofacturaSelect).subscribe(
+            data => this.facturas = data
+          )
+          this.desdeFecha = null;
+          this.hastaFecha = null;
+          this.proveedorSelect = 0;
+          this.cobradoSelect = 0;
+        } else {
+          this.getAll()
+        }
+        break;
+      case 'desdeFecha':
+        if (this.hastaFecha) {
+          this.sql.filtroFecha(`${moment(this.desdeFecha).format("YYYY-MM-DD")}`, `${moment(this.hastaFecha).format("YYYY-MM-DD")}`).subscribe(
+            data => this.facturas = data
+          )
+          this.tipofacturaSelect = 0;
+          this.proveedorSelect = 0;
+          this.cobradoSelect = 0;
+        }
+        break;
+      case 'hastaFecha':
+        if (this.desdeFecha) {
+          this.sql.filtroFecha(`${moment(this.desdeFecha).format("YYYY-MM-DD")}`, `${moment(this.hastaFecha).format("YYYY-MM-DD")}`).subscribe(
+            data => this.facturas = data
+          )
+          this.tipofacturaSelect = 0;
+          this.proveedorSelect = 0;
+          this.cobradoSelect = 0;
+        }
+        break;
+      case 'proveedor':
+        if (this.proveedorSelect > 0) {
+          this.sql.filtroGeneral('id_proveedor', this.proveedorSelect).subscribe(
+            data => this.facturas = data
+          )
+          this.tipofacturaSelect = 0;
+          this.desdeFecha = null;
+          this.hastaFecha = null;
+          this.cobradoSelect = 0;
+        } else {
+          this.getAll()
+        }
+        break;
+    }
+  }
+
+  pdf(data: FacturaProveedorInterface){
+    this.pdfService.newPdf(data);
+  }
+
+  reiniciarFiltros(){
+    this.tipofacturaSelect = 0;
+    this.desdeFecha = null;
+    this.hastaFecha = null;
+    this.proveedorSelect = 0;
+    this.cobradoSelect = 0;
+    this.getAll();
   }
 
 }
--- src/app/usuario/usuario.module.ts
+++ src/app/usuario/usuario.module.ts
@@ -8,19 +8,26 @@ import { InicioComponent } from './inicio/inicio.component';
 import { FacturasComponent } from './facturas/facturas.component';
 import { PerfilComponent } from './perfil/perfil.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { IncidenciaComponent } from './incidencia/incidencia.component';
+import { ApplicationPipesModule } from '../pipes/pipes.module';
+
+//PrimeNg
+import { ProgressSpinnerModule } from 'primeng/progressspinner';
+import { BlockUIModule } from 'primeng/blockui';
+import { DialogModule } from 'primeng/dialog';
+import { ButtonModule } from 'primeng/button';
+import { ToastModule } from 'primeng/toast';
+import { OverlayPanelModule } from 'primeng/overlaypanel';
 import { MessagesModule } from 'primeng/messages';
 import { MessageModule } from 'primeng/message';
-import { ToastModule } from 'primeng/toast';
-import { IncidenciaComponent } from './incidencia/incidencia.component';
 import { ScrollPanelModule } from 'primeng/scrollpanel';
-import { DialogModule } from 'primeng/dialog';
-import { ProgressSpinnerModule } from 'primeng/progressspinner';
+import { TooltipModule } from 'primeng/tooltip';
 import { CalendarModule } from 'primeng/calendar';
-import { ApplicationPipesModule } from '../pipes/pipes.module';
+import { ChartModule } from 'primeng/chart';
 
 @NgModule({
   declarations: [UsuarioComponent, HeaderUsuarioComponent, InicioComponent, FacturasComponent, PerfilComponent, IncidenciaComponent],
-  imports: [RouterModule.forRoot(routes), BrowserModule,FormsModule, ReactiveFormsModule, MessageModule, MessagesModule, ToastModule, ScrollPanelModule, DialogModule, ProgressSpinnerModule, CalendarModule, ApplicationPipesModule],
+  imports: [RouterModule.forRoot(routes), BrowserModule,FormsModule, ReactiveFormsModule,ChartModule,OverlayPanelModule,TooltipModule,ButtonModule,BlockUIModule, MessageModule, MessagesModule, ToastModule, ScrollPanelModule, DialogModule, ProgressSpinnerModule, CalendarModule, ApplicationPipesModule],
   exports: []
 })
 export class UsuarioModule { }
